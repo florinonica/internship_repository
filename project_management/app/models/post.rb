84c54ff2b8c45f1ebf4e13dc037963b0de2daba3
florinonica <onicaflorin96@gmail.com>
2018-08-14 13:15:51 +0300

    Chat views

--- project_management/app/models/post.rb
+++ project_management/app/models/post.rb
@@ -4,4 +4,21 @@ class Post < ApplicationRecord
   has_many :attachments, :as => :container, dependent: :destroy
   validates :body, :presence => true, length: { maximum: 200 }
   accepts_nested_attributes_for :attachments
+
+  def get_color
+  	user = User.find(user_id)
+  	if user.is_a? Client
+  	  return "#0079bf"
+  	elsif user.is_a? Employee
+  	  return "#F80602"
+  	else 
+  	  return "#F5E106"
+  	end
+  	return "white"
+  end
+
+  def is_owned?(user)
+  	(user.id == user_id ? true : false)
+  end
+  
 end
