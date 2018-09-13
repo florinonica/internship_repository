e0696d22db505e372c0bd780bd9a07d25331020b
florinonica <onicaflorin96@gmail.com>
2018-09-13 10:43:16 +0300

    Added option to make report available to client

--- project_management/db/schema.rb
+++ project_management/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_073104) do
+ActiveRecord::Schema.define(version: 2018_09_13_074126) do
 
   create_table "attachments", force: :cascade do |t|
     t.string "file_file_name"
@@ -114,13 +114,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_073104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "report_data"
-  end
-
-  create_table "reports_users", id: false, force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "report_id", null: false
-    t.index ["report_id"], name: "index_reports_users_on_report_id"
-    t.index ["user_id"], name: "index_reports_users_on_user_id"
+    t.boolean "available_to_clients"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -177,6 +171,13 @@ ActiveRecord::Schema.define(version: 2018_09_13_073104) do
     t.integer "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_reports", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "report_id", null: false
+    t.index ["report_id"], name: "index_reports_users_on_report_id"
+    t.index ["user_id"], name: "index_reports_users_on_user_id"
   end
 
   create_table "versions", force: :cascade do |t|
