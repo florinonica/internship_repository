1a373f0c91c2377d4d76170cf921b1fd028b0ceb
florinonica <onicaflorin96@gmail.com>
2018-11-05 17:11:38 +0200

    removed stuff

--- blog/app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ApplicationRecord
-  has_many :comments, dependent: :destroy
-  belongs_to :user
-  mount_uploader :attachment, AttachmentUploader
-  validates :title, presence: true, length: { minimum: 5 }
-  validates :text, presence: true, length: { minimum: 50 }
-  validates_format_of :text, without: /<script/, message: "You are not allowed to post JS"
-end
--- blog/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
-  resources :articles do
-    resources :comments
-  end
-  root 'welcome#index'
-end
